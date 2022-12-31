f7479b451e9388d851a4722d18d761a0ae384edc
kosuha <okeydokekim@gmail.com>
2022-12-31 17:11:08 +0100

    font

--- notion.py
+++ notion.py
@@ -39,6 +39,9 @@ def get_page(url):
 	<head>
 		<meta charset="UTF-8">
 		<title>NOTION GOD</title>
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 		<link rel="stylesheet" href="/static/css/style.css">
 	</head>
 	<body>
